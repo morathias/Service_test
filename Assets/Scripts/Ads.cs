def4b6deaff5fd239c24ba6c6b6f22a191dee28f
morathias <matias_morales_1996@hotmail.com>
2017-04-24 22:26:40 -0300

    Ads

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ads : MonoBehaviour {
+
+	void Awake(){
+		Advertisement.Initialize ("1395370");
+	}
+
+	void Update(){
+		if (!Advertisement.isShowing)
+			Time.timeScale = 1;
+	}
+
+	public void playAd(){
+		Time.timeScale = 0;
+		StartCoroutine (ShowAdWhenReady());
+	}
+
+	IEnumerator ShowAdWhenReady(){
+		while (!Advertisement.IsReady())
+			yield return null;
+
+		Advertisement.Show ();
+	}
+
+	void AdCallbackhandler (ShowResult result){
+		switch (result) {
+		case ShowResult.Finished:
+			PointsManager.updatePoints (5f);
+			break;
+		case ShowResult.Skipped:
+			Debug.Log ("Ad skipped. Son, I am dissapointed in you");
+			break;
+		}
+	}
+}
